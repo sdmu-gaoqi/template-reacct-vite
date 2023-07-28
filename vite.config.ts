f880c753a9fff4f1d114074a6c6a61f26b4d8e38
gaoqi <1224362143@qq.com>
2023-07-28 11:57:28 +0800

    chore: 更新模板

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,44 @@ import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 import mkcert from 'vite-plugin-mkcert'
+import viteImagemin from 'vite-plugin-imagemin'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [
+    react(),
+    mkcert(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false
+      },
+      optipng: {
+        optimizationLevel: 7
+      },
+      mozjpeg: {
+        quality: 20
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox'
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
