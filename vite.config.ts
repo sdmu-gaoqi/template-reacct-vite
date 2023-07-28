99f1ecd9ac02a365c4d9aa387b27674258a75d62
gaoqi <1224362143@qq.com>
2023-07-28 15:37:11 +0800

    chore: alias不生效

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import react from '@vitejs/plugin-react'
-import path from 'path'
-// import { fileURLToPath } from 'url'
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import mkcert from 'vite-plugin-mkcert'
 import viteImagemin from 'vite-plugin-imagemin'
-// import { dirname } from 'path'
-
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = dirname(__filename)
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -40,17 +36,15 @@ export default defineConfig({
           }
         ]
       }
-    })
+    }),
+    tsconfigPaths()
   ],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      hooks: path.resolve(__dirname, './src/hooks'),
-      styles: path.resolve(__dirname, './src/styles'),
-      pages: path.resolve(__dirname, './src/pages'),
-      components: path.resolve(__dirname, './src/components'),
-      mocks: path.resolve(__dirname, './mocks')
-    }
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: 'store', replacement: path.resolve(__dirname, './src/store') }
+    ]
   },
   server: {
     https: true
